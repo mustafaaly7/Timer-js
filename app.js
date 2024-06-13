a5a2ca18ed26f9e3f198b75361c33c5305d48243
Mustafa Ali <mustafaaaly27@gmail.com>
2024-06-13 18:02:53 +0500

    timer Without styling

---
+++ app.js
@@ -0,0 +1,64 @@
+var input ;
+function getData(){
+
+     input = +prompt("Enter Your Timer in minutes")-1
+    minute.innerHTML = input
+
+}
+
+var minute = document.getElementById("minute")
+var second = document.getElementById("second")
+var sec = 59
+var interval ;
+var startbtn =document.getElementById("startbtn")
+function startTimer(){
+    // interval = setInterval(start,1000)
+    
+    sec --
+    second.innerHTML = sec
+    if(sec == 0){
+        input -- 
+        minute.innerHTML -- 
+        sec = 59
+        sec.innerHTML = sec
+
+    }if(minute.innerHTML && second.innerHTML == 0){
+        clearInterval(interval)
+        alert("stopped")
+        minute.innerHTML = 0
+        second.innerHTML = 0
+    }
+    // ele.setAttribute("disabled","true")
+}
+
+
+
+function start(ele){
+    if(input >= 1){
+        interval = setInterval(startTimer,1000)
+        startbtn.setAttribute("disabled","true")
+
+    }else{
+        alert("Enter Valid Number")
+        return
+    }
+
+}
+
+
+function stop(){
+    clearInterval(interval)
+    startbtn.removeAttribute("disabled")
+}
+
+
+function reset(){
+clearInterval(interval)
+startbtn.removeAttribute("disabled")
+
+minute.innerHTML = 0
+second.innerHTML = 0
+
+
+getData()
+}
